d27da2ef021ce8ffc3c7277194f0ef55ce2f0b18
Evgenii Sumaev <9364016@gmail.com>
2019-11-01 12:34:16 +0300

    Prerelease

--- routes/login.js
+++ routes/login.js
@@ -8,14 +8,17 @@ const router = Router();
 // login
 router.post('/', async (req, res) => {
   const { email, password } = req.body;
-  if (!email || !password) return res.json({ msg: 'Please fill all fields' })
+  if (!email || !password) return res.json({ msg: 'Please fill all fields' });
   const user = await User.findOne({ email });
   if (!user) return res.json({ msg: 'No user found' });
   if (user.password === password) {
-    const token = await jwt.sign({
-      id: user.id
-    }, 'myJwtSecret',
-    { expiresIn: 3600 });
+    const token = await jwt.sign(
+      {
+        id: user.id
+      },
+      'myJwtSecret',
+      { expiresIn: 3600 }
+    );
     return res.json({ user, token });
   }
 });
