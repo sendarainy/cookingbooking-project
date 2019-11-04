44b1c921279ed136706f201ac75e02ea647273a0
noneinnon <antbrw@gmail.com>
2019-11-04 15:07:09 +0300

    fixed more conf

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import 'materialize-css/dist/css/materialize.css'
 import 'materialize-css/dist/js/materialize'
-import { Navbar, NavItem } from 'react-materialize'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import Home from './components/pages/Home'
 import Header from './components/Header'
-import LoginModal from './components/auth/LoginModal'
-import SignupModal from './components/auth/SignupModal'
+import Map from './components/Map/Map';
+import AddVenue from './components/AddVenue/AddVenue';
+import FilterBar from './components/FilterBar/FilterBar';
+
 import { loadUser } from './actions/authActions'
 
 
@@ -22,10 +23,13 @@ export default class App extends Component {
       <Provider store={store} className='App'>
         <Router>
           <Header />
+          <FilterBar />
           <Switch>
             <Route path='/' exact component={Home} />
+            <Route exact path='/venues/new' component={AddVenue} />
             {/* <Route path='/login' component={Login} /> */}
           </Switch>
+          <Route exact path='/' component={Map} />
         </Router>
       </Provider>
     );
