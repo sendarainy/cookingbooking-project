2a02751b13cef08ccad959fc8bd78d83cb27dccc
noneinnon <antbrw@gmail.com>
2019-11-08 14:03:45 +0300

    filter fix

--- client/src/components/FilterBar/FilterBar.jsx
+++ client/src/components/FilterBar/FilterBar.jsx
@@ -18,7 +18,22 @@ class FilterBar extends Component {
     hours: null,
     price: null
   }
-
+  dropFilter = () => {
+    this.setState(
+      {
+        date: null,
+        options: {
+          pastry: false,
+          gastro: false,
+          cold: false
+        },
+        parsedDate: null,
+        hours: null,
+        price: null
+      }
+    )
+    this.props.cancelFilter()
+  }
   onChangeOptions = async e => {
     await this.setState({
       options: {
@@ -57,24 +72,27 @@ class FilterBar extends Component {
   };
   render() {
     return (
+      <Row className='container'>
       <div className='filterBar'>
-        <Flatpickr
-              data-enable-time
-              value='Дата'
+        <Col s={2} className='myDate'>
+        <Flatpickr style={{ borderBottom: '1px solid #9e9e9e'}}
+              data-enable-time 
+              defaultValue={this.state.date}
               onChange={(date) => this.handleDateChange(date)}
-        /> 
+        />
+        </Col>
         <Select name='price' onChange={this.handleChangePrice}>
-          <option value='' disabled>Цена</option>
-          <option value="1000">До 1000</option>
-          <option value="2000">До 2000</option>
-          <option value="3000">До 3000</option>
+          <option value=''></option>
+          <option value="1000">До 1000 р.</option>
+          <option value="2000">До 2000 р.</option>
+          <option value="3000">До 3000 р.</option>
         </Select>
         <div className='checkContainer' style={{ display: 'flex', alignItems: 'center'}}>
            <div style={{marginLeft: '10px'}}>
              <Checkbox
                className='pastry'
                onChange={this.onChangeOptions}
-               value='false'
+               value={this.state.pastry}
                label='Кондитерский цех'
                checked={this.state.options.pastry}
              />
@@ -83,7 +101,7 @@ class FilterBar extends Component {
              <Checkbox
                className='gastro'
                onChange={this.onChangeOptions}
-               value='false'
+               value={this.state.gastro}
                label='Кулинарный цех'
                checked={this.state.options.gastro}
              />
@@ -92,14 +110,15 @@ class FilterBar extends Component {
              <Checkbox
                className='cold'
                onChange={this.onChangeOptions}
-               value='false'
+               value={this.state.cold}
                label='Холодный цех'
                checked={this.state.options.cold}
              />
            </div>
          </div>
-        <Button icon={<Icon>close</Icon>}onClick={this.props.cancelFilter} className='button red lighten-2' tooltip='Сбросить фильтр'></Button>
+        <Button icon={<Icon>close</Icon>}onClick={this.dropFilter} className='button red lighten-2' tooltip='Сбросить фильтр'></Button>
       </div>
+      </Row>
     )
   }
 }
